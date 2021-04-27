e58a00c2ed0565e70d3cf654fcc69f1b62ed61a6
Rodney Lab <codesign@rodneylab.com>
2021-04-27 17:51:32 +0100

    fix: updated CSP headings

--- gatsby-config.js
+++ gatsby-config.js
@@ -77,6 +77,7 @@ module.exports = {
           '/': [
             'Permissions-Policy: accelerometer=(), autoplay=(), camera=(), document-domain=(), encrypted-media=(), fullscreen=(), gyroscope=(), interest-cohort=(), magnetometer=(), microphone=(), midi=(), payment=(), picture-in-picture=(), publickey-credentials-get=(), sync-xhr=(), usb=(), xr-spatial-tracking=(), geolocation=()',
             'Strict-Transport-Security: max-age=31536000; includeSubDomains; preload',
+            'X-Robots-Tag "index"',
           ],
         },
         // headers: [ 'Permissions-Policy: accelerometer=(), autoplay=(), camera=(), document-domain=(), encrypted-media=(), fullscreen=(), gyroscope=(), interest-cohort=(), magnetometer=(), microphone=(), midi=(), payment=(), picture-in-picture=(), publickey-credentials-get=(), sync-xhr=(), usb=(), xr-spatial-tracking=(), geolocation=()',
